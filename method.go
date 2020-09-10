ed1478f732a9adcb61677f9402a73c19d868d7b2
Leo-Guo <2505694023@qq.com>
2020-09-10 16:07:25 +0800

    错误处理

--- method.go
+++ method.go
@@ -1,7 +1,6 @@
 package twins
 
 import (
-	"github.com/Leo-Guo/twins/common"
 	"github.com/Leo-Guo/twins/service/compare"
 	"github.com/Leo-Guo/twins/service/request"
 	"github.com/kataras/iris/v12"
@@ -14,7 +13,7 @@ func myMiddleware(ctx iris.Context) {
 	ctx.Next()
 }
 
-func SendRequest(service string,url string,method string,req interface{},res interface{}) (common.ErrorCode, []byte){
+func SendRequest(service string,url string,method string,req interface{},res interface{}) (error, []byte){
 	return  request.Send(service, url, method, req, res)
 }
 
--- service/request/request.go
+++ service/request/request.go
@@ -1,7 +1,6 @@
 package request
 
 import (
-	"github.com/Leo-Guo/twins/common"
 	"github.com/Leo-Guo/twins/components/http"
 	jsoniter "github.com/json-iterator/go"
 	"strings"
@@ -16,29 +15,29 @@ type RpcConfig struct {
 
 const DefaultTimeout = 3 * time.Second
 
-func Send(service string, url string, method string, req interface{}, res interface{}) (common.ErrorCode, []byte) {
+func Send(service string, url string, method string, req interface{}, res interface{}) (error, []byte) {
 	// 序列化 post_body
 	reqBody, errMarshal := jsoniter.Marshal(req)
 	if errMarshal != nil {
-		return common.ERROR_MARSHAL, nil
+		return errMarshal, nil
 	}
 	if strings.EqualFold(method, "POST") {
 		response, errReq := http.PostJson(service, url, reqBody, DefaultTimeout)
 		if errReq != nil {
-			return common.ERROR_REQUEST_UNKNOW, nil
+			return errReq, nil
 		}
 		if response.StatusCode == 500 {
-			return common.ERROR_REQUEST_INTERNAL_ERROR_500, response.Body
+			return errReq, response.Body
 		}
 		if response.StatusCode == 404 {
-			return common.ERROR_REQUEST_INTERNAL_ERROR_500, response.Body
+			return errReq, response.Body
 		}
 		// 反序列化 response_body
 		errUnMarshal := jsoniter.Unmarshal(response.Body, res)
 		if errUnMarshal != nil {
-			return common.ERROR_UNMARSHAL, response.Body
+			return errReq, response.Body
 		}
-		return common.ERROR_OK, response.Body
+		return errReq, response.Body
 	}
-	return common.ERROR_REQUEST_UNKNOW, []byte{}
+	return nil, []byte{}
 }
